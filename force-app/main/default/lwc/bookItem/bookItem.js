dc397d9e490cf17f2c53334408bbc5c25c888ddf
Ivan <denatarelcom2105@gmail.com>
2022-10-26 13:36:51 +0300

    removed try catch in bookItem

--- force-app/main/default/lwc/bookItem/bookItem.js
+++ force-app/main/default/lwc/bookItem/bookItem.js
@@ -2,43 +2,33 @@ import { LightningElement, api } from "lwc";
 import { ShowToastEvent } from "lightning/platformShowToastEvent";
 import buyBook from "@salesforce/apex/BookController.buyBook";
 import Id from "@salesforce/user/Id";
-// reportValidity, checkValidity
+
 export default class BookItem extends LightningElement {
   @api book;
   quantityToBuy = 1;
   userId = Id;
 
   buyBook() {
-    try {
-      if (this.book.Quantity__c - this.quantityToBuy < 0) {
-        throw new Error(
-          "Exceeding number of books. You cant buy more than available in stock."
+    const payload = {
+      bookId: this.book.Id,
+      userId: this.userId,
+      bookQty: this.quantityToBuy,
+      price: this.book.Price__c,
+      bookstoreId: this.book.Bookstore__r.Id
+    };
+
+    buyBook({ payload: JSON.stringify(payload) })
+      .then(() => {
+        this._showToaster(
+          "Success",
+          `Bought new book: ${this.book.Book_Title__c}`,
+          "success"
         );
-      }
-
-      const payload = {
-        bookId: this.book.Id,
-        userId: this.userId,
-        bookQty: this.quantityToBuy,
-        price: this.book.Price__c,
-        bookstoreId: this.book.Bookstore__r.Id
-      };
-
-      buyBook({ payload: JSON.stringify(payload) })
-        .then(() => {
-          this._showToaster(
-            "Success",
-            `Bought new book: ${this.book.Book_Title__c}`,
-            "success"
-          );
-          this.dispatchEvent(new CustomEvent("buybook"));
-        })
-        .catch((err) => {
-          this._showToaster("Error!", err.body.message, "error");
-        });
-    } catch (err) {
-      this._showToaster("Error!", err.message, "error");
-    }
+        this.dispatchEvent(new CustomEvent("buybook"));
+      })
+      .catch((err) => {
+        this._showToaster("Error!", err.body.message, "error");
+      });
   }
 
   handleQuantityChange(event) {
